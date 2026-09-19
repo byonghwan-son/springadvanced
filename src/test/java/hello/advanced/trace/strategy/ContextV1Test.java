1558b66c0959291f3b2d1f16f8b90764a3b6357b
sonstein <sonstein@daum.net>
2026-09-20 00:16:38 +0900

    전략 패턴

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,73 @@
+package hello.advanced.trace.strategy;
+
+
+import hello.advanced.trace.strategy.code.ContextV1;
+import hello.advanced.trace.strategy.code.Strategy;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+  @Test
+  void templateMethodV0() {
+    logic1();
+    logic2();
+  }
+
+  private void logic1() {
+    long startTime = System.currentTimeMillis();
+    // 비즈니스 로직 실행
+    log.info("비즈니스 로직1 실행");
+    // 비즈니스 로직 종료
+    long endTime = System.currentTimeMillis();
+    long resultTime = endTime - startTime;
+    log.info("resultTime={}", resultTime);
+  }
+
+  private void logic2() {
+    long startTime = System.currentTimeMillis();
+    // 비즈니스 로직 실행
+    log.info("비즈니스 로직2 실행");
+    // 비즈니스 로직 종료
+    long endTime = System.currentTimeMillis();
+    long resultTime = endTime - startTime;
+    log.info("resultTime={}", resultTime);
+  }
+
+  @Test
+  void strategyV1() {
+    ContextV1 context = new ContextV1(new StrategyLogic1());
+    context.execute();
+
+    context = new ContextV1(new StrategyLogic2());
+    context.execute();
+  }
+
+  @Test
+  void strategyV2() {
+    Strategy strategyLogic1 = () -> log.info("비즈니스 로직1 실행");
+    log.info("strategyLogic1={}", strategyLogic1);
+    new ContextV1(strategyLogic1).execute();
+
+    Strategy strategyLogic2 = () -> log.info("비즈니스 로직2 실행");
+    log.info("strategyLogic2={}", strategyLogic2);
+    new ContextV1(strategyLogic2).execute();
+  }
+
+  @Test
+  void strategyV3() {
+    ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+    context1.execute();
+
+    ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+    context2.execute();
+  }
+
+  @Test
+  void strategyV4() {
+    new ContextV1(() -> log.info("비즈니스 로직1 실행")).execute();
+    new ContextV1(() -> log.info("비즈니스 로직2 실행")).execute();
+  }
+}
